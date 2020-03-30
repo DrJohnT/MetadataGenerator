d8162d97a7ee8cc84ec9ec2d209433d6e091465d
Dr John Tunnicliffe <john@bovi.co.uk>
2020-03-30 21:57:09 +0100

    Added bulk manipulation of cube structure

---
+++ CSharp/GenerateCube/ExportCubeDefinitionToDisk.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Text;
+
+namespace GenerateCube
+{
+    class ExportCubeDefinitionToDisk
+    {
+        /// <summary>
+        /// Extracts the deployed cube structure from the deployed instance and outputs it to a folder
+        /// </summary>
+        /// <param name="db"></param>
+        public static void ExportCubeDefinition(Microsoft.AnalysisServices.Tabular.Database db)
+        {
+            string outputDir = Path.Combine(Properties.Settings.Default.OutputDir, Properties.Settings.Default.AsDatabaseId);
+            CreateOrCleanDirectory(outputDir);
+
+            var dbJson = Microsoft.AnalysisServices.Tabular.JsonScripter.ScriptCreateOrReplace(db);
+            string filePath = Path.Combine(outputDir, "CreateOrReplace.json");
+            File.WriteAllText(filePath, dbJson);
+            
+            int counter = 0;
+            int totalLines;
+
+            string line;
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    counter++;
+                }
+                totalLines = counter;
+                file.Close();
+            }
+
+
+
+            StringBuilder newDb = new StringBuilder();
+            counter = 0;
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (counter == 0)
+                    {
+                        newDb.AppendLine(line);
+                    } 
+                    else 
+                    {
+                        if (counter > 5 && counter < totalLines - 2)
+                        {
+                            // strip off first few white space characters so the file formats match
+                            newDb.AppendLine(line.Substring(4));
+                        }
+                    }
+                    counter++;
+                }
+                file.Close();
+            }
+
+            
+           
+
+            filePath = Path.Combine(outputDir, "Model.bim");
+
+            File.WriteAllText(filePath, newDb.ToString());
+        }
+
+
+
+        public static void CreateOrCleanDirectory(string outputDir)
+        {
+            DirectoryInfo directory = new DirectoryInfo(outputDir);
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            // clean the output folder so we don't have old views hanging around
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+
+    }
+}
---
+++ CSharp/GenerateCube/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.AnalysisServices.Tabular;
+
+namespace GenerateCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Server svr = new Server())
+            {
+                svr.Connect(Properties.Settings.Default.AsServerName);
+
+                Database db = svr.Databases.FindByName(Properties.Settings.Default.AsDatabaseId);
+                if (db != null)
+                { 
+                    Console.WriteLine(db.CompatibilityLevel);
+                    Model m = db.Model;
+
+                    foreach (Table table in m.Tables)
+                    {
+                        foreach (Column col in table.Columns)
+                        {
+                            if (col.Name.ToLower().EndsWith("key"))
+                            {
+                                // hide key columns
+                                col.IsHidden = true;
+                            } 
+                            else if (col.Name.StartsWith("m_"))
+                            {
+                                // hide the original column
+                                col.IsHidden = true;
+
+                                string measureName = SplitCamelCase(col.Name.Substring(2));
+
+                                // check if the measure already exists 
+                                Measure measure = table.Measures.Find(measureName);
+                                if (measure == null)
+                                {
+                                    // the measure does not exist, so add it
+                                    Console.WriteLine("Creating measure {0}", measureName);
+                                    measure = new Measure();
+                                    measure.Name = measureName;
+                                    measure.Expression = string.Format(" sum({0}[{1}])", table.Name, col.Name);
+                                    measure.FormatString = GetMeasureFormatString(col.Name);
+                                    
+                                    // add the new measure to the cube
+                                    table.Measures.Add(measure);
+                                }
+                                else
+                                {
+                                    // set the format string (in case we missed it last time)
+                                    measure.FormatString = GetMeasureFormatString(col.Name);
+                                }
+                            }
+                            else if (col.Type.ToString() == "Data")
+                            {
+                                // camel case other Data columns
+                                 col.Name = SplitCamelCase(col.Name);
+                            }
+                        }
+                    }
+                    
+                    // save the new measures and renamed columns to the server
+                    db.Update(Microsoft.AnalysisServices.UpdateOptions.ExpandFull);
+
+                    // export the cube definition to the file system so we can diff using Beyond Compare
+                    ExportCubeDefinitionToDisk.ExportCubeDefinition(db);
+
+                }
+                svr.Disconnect();
+            }
+         
+            Console.WriteLine("Done!");
+            //Console.ReadKey();
+        }
+
+        private static string GetMeasureFormatString(string columnName)
+        {
+            const string CurrencyFormatString = "\"\"#,0;-\"\"#,0;\"\"#,0";
+            const string WholeNumberFormatString = "\"\"#,0;-\"\"#,0;\"\"#,0";
+            if (columnName.ToLower().IndexOf("count") > 0)
+            {
+                return WholeNumberFormatString;
+            }
+            else
+            {
+                return CurrencyFormatString;
+            }
+        }
+       
+        #region General Helper functions
+        private static string GetProjectDir()
+        {
+            return Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+        }
+
+        public static string SplitCamelCase(string str)
+        {
+            string returnValue = Regex.Replace(
+                Regex.Replace(
+                    str,
+                    @"(\P{Ll})(\P{Ll}\p{Ll})",
+                    "$1 $2"
+                ),
+                @"(\p{Ll})(\P{Ll})",
+                "$1 $2"
+            );
+            return returnValue.Replace("_", "").Replace("  ", " ");
+        }
+        #endregion
+    }
+}
